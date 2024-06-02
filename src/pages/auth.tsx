0475dece4c52f4eb673a97c1ad73c95385373d2b
filaretovdmitriy <filaretovd@yandex.ru>
2024-06-02 09:17:08 +0300

    added tailwind form

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,16 +1,31 @@
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import "../assets/css/tailwind.css";
 const AuthPage = () => {
-    return (
-        <div>
-            <h1>Auth</h1>
-            <Button>Click me</Button>
+  return (
+    <>
+      <header className="container mx-auto">
+        <div className="flex flex-row items-center justify-between pt-8 pb-8">
+          <span>Логотип</span>
+          <a href="#">Перейти на сайт</a>
         </div>
-    )
-}
+      </header>
+      <main className="container">
+        <div className="w-full max-w-xs">
+          <h1 className="text-base mb-4 font-bold">Введите имя и пароль</h1>
+          <div className="mb-4">
+            <Input type="email" placeholder="E-mail" />
+          </div>
+          <div className="mb-4">
+            <Input type="password" placeholder="Пароль" />
+          </div>
+          <div className="mb-4">
+            <Button>Войти</Button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
 
-export {AuthPage}
-
-
-
-
+export { AuthPage };
